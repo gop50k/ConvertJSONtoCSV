c9732a5cf2576485d8fd16e7a3709d3da54407f9
Go nakagawa <g.nakagawa@gemcook.com>
2019-01-16 22:19:43 +0900

    構造体のキーの取得まで実装

--- convertJtoC.go
+++ convertJtoC.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"reflect"
 )
 
 // Information はJSONファイルの構造体です。
@@ -15,7 +16,49 @@ type Information struct {
 	StartAt string `json:"start_at"`
 }
 
+func sub() {
+	lenInfo := Information{}
+	fmt.Println(lenInfo)
+
+	leninfo := StructToMap(&lenInfo)
+	fmt.Println(leninfo)
+}
+
+// StructToMap は
+func StructToMap(data interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	elem := reflect.ValueOf(data).Elem()
+	size := elem.NumField()
+
+	for i := 0; i < size; i++ {
+		field := elem.Type().Field(i).Name
+		value := elem.Field(i).Interface()
+		result[field] = value
+	}
+
+	return result
+}
+
 func main() {
+
+	// 下で使うfor文のためにStructの要素数を取得
+	// result := make(map[string]interface{})
+	// elem := reflect.ValueOf(data).Elem()
+	// size := elem.NumField()
+
+	// type information []Information
+	// var lenInfo information
+
+	// fmt.Println(len(lenInfo))
+
+	// Structのjsonタグを取得
+	s := Information{}
+	t := reflect.TypeOf(s)
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		fmt.Println(field.Tag.Get("json"))
+	}
+
 	// JSONファイル読み込み
 	bytes, err := ioutil.ReadFile("./input.json")
 	if err != nil {
@@ -27,6 +70,9 @@ func main() {
 	if err := json.Unmarshal(bytes, &Informations); err != nil {
 		log.Fatal(err)
 	}
+
+	// JSONデコードした結果を表示
+	// fmt.Println(Informations)
 
 	//CSVファイル作成
 	file, err := os.OpenFile("./output.csv", os.O_WRONLY|os.O_CREATE, 0600)
@@ -51,3 +97,18 @@ func main() {
 	}
 	fmt.Println("処理終了")
 }
+
+// 	result := make(map[string]interface{})
+// 	elem := reflect.ValueOf(data).Elem()
+// 	size := elem.NumField()
+
+// 	for i := 0; i < size; i++ {
+// 	  field := elem.Type().Field(i).Tag.Get("json")
+// 	  value := elem.Field(i).Interface()
+// 	  result[field] = value
+
+//   c := StructToJsonTagMap(&Informations)
+//   fmt.Println(c)
+
+// 実行コマンドショートカット
+// go run convertJtoC.go
